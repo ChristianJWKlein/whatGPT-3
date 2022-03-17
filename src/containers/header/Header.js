228c5d8b3d9a3cdd687eb49d97b926c8dc607909
Christian JW Klein <cklein010715@gmail.com>
2022-03-16 23:17:02 -0400

    test

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import './header.css';
+import { useState } from 'react';
+
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowRight,
+} from './headerCss';
+import Video from './video.mp4';
 
 export default function Header() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
-    <>
-      <section>Header</section>
-    </>
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Events Made Easy</HeroH1>
+        <HeroP>See all the events happening right now!</HeroP>
+        <HeroBtnWrapper>
+          <button onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started {<ArrowRight />}
+          </button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
   );
 }
